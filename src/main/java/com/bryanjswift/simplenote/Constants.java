2495ba5fd2907eafc540c5c0f413d45e71025207
Bryan J Swift <bryan.j.swift@gmail.com>
2011-03-27 22:53:17 -0400

    Remove as yet unimplemented API endpoints from Constants
    Add API2 endpoint for creating notes.

--- src/main/java/com/bryanjswift/simplenote/Constants.java
+++ src/main/java/com/bryanjswift/simplenote/Constants.java
@@ -5,14 +5,10 @@ public class Constants {
     public static final String TAG = "SimplenoteApi";
     // API Base URL
     public static final String BASE_URL = "https://simple-note.appspot.com";
-    public static final String CREATE_ACCOUNT_URL = BASE_URL + "/createaccount.html";
-    public static final String API_BASE_URL   = BASE_URL + "/api";
-    public static final String API_LOGIN_URL  = API_BASE_URL + "/login";                // POST
-    public static final String API_REGISTER_URL = BASE_URL + "/create";                 // POST
-    public static final String API_NOTES_URL  = API_BASE_URL + "/index";                // GET
-    public static final String API_NOTE_URL   = API_BASE_URL + "/note";                 // GET
-    public static final String API_UPDATE_URL = API_BASE_URL + "/note";                 // POST
-    public static final String API_DELETE_URL = API_BASE_URL + "/delete";               // GET
-    public static final String API_SEARCH_URL = API_BASE_URL + "/search";               // GET
+    public static final String API_BASE_URL = BASE_URL + "/api";
+    public static final String API2_BASE_URL = BASE_URL + "/api2";
+    public static final String API_LOGIN_URL  = API_BASE_URL + "/login";
+    public static final String API_REGISTER_URL = BASE_URL + "/create";
+    public static final String API_NOTE_CREATE_URL = API2_BASE_URL + "/data?auth=%s&email=%s";
     private Constants() { }
 }
