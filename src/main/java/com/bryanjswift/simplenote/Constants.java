b551667429dffaaad9ac37428af4a5f208a1bde9
Bryan J Swift <bryan.j.swift@gmail.com>
2011-04-08 21:20:43 -0400

    Add EMPTY_LIST constant so one doesn't have to be created each time

--- src/main/java/com/bryanjswift/simplenote/Constants.java
+++ src/main/java/com/bryanjswift/simplenote/Constants.java
@@ -1,10 +1,16 @@
 package com.bryanjswift.simplenote;
 
+import com.bryanjswift.simplenote.model.Note;
+import com.google.common.collect.ImmutableList;
+
+import java.util.List;
+
 /** @author bryanjswift */
 public class Constants {
     // Defaults
     public static final String DEFAULT_KEY = "__SN_DEFAULT_KEY__";
     public static final int DEFAULT_VERSION = -12389;
+    public static final List<Note> EMPTY_LIST = (new ImmutableList.Builder<Note>()).build();
     // API Base URL
     public static final String BASE_URL = "https://simple-note.appspot.com";
     public static final String API_BASE_URL = BASE_URL + "/api";
@@ -12,5 +18,6 @@ public class Constants {
     public static final String API_LOGIN_URL  = API_BASE_URL + "/login";
     public static final String API_REGISTER_URL = BASE_URL + "/create";
     public static final String API_NOTE_CREATE_URL = API2_BASE_URL + "/data?auth=%s&email=%s";
+    public static final String API_LIST_URL = API2_BASE_URL + "/index?auth=%s&email=%s";
     private Constants() { }
 }
