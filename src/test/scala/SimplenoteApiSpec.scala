493f13e554732c115e265f0ab0b9351a82e7e0fd
Bryan J Swift <bryan.j.swift@gmail.com>
2011-04-08 23:04:02 -0400

    Change to use more explqnatory varialbe names

--- src/test/scala/SimplenoteApiSpec.scala
+++ src/test/scala/SimplenoteApiSpec.scala
@@ -9,18 +9,18 @@ class SimplenoteApiSpec extends WordSpec with ShouldMatchers {
   val userAgent = "SimplenoteJavaApiTest/1.0"
   val credentials = new Credentials(null, "swiftnote@bryanjswift.com", "simplenote1234")
   val badCredentials = new Credentials(null, "swiftnote@bryanjswift.com", "bad password");
-  val api = new SimplenoteApi(userAgent)
+  val unauthorized = new SimplenoteApi(userAgent)
 
   "SimplenoteApi" should {
     "return Credentials with valid auth value on successful login" in {
-      val response = api.login(credentials.email, credentials.password)
+      val response = unauthorized.login(credentials.email, credentials.password)
       response.status should be (HttpStatus.SC_OK)
       response.payload.email should be (credentials.email)
       response.payload.auth.length should be > 0
       response.payload.password should be (null)
     }
     "return with unauthorized status on unsuccessful login" in {
-      val response = api.login(badCredentials.email, badCredentials.password)
+      val response = unauthorized.login(badCredentials.email, badCredentials.password)
       response.status should not be (HttpStatus.SC_UNAUTHORIZED)
       response.payload.email should be (badCredentials.email)
       response.payload.password should be (null)
