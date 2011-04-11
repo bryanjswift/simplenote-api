054b0f00a6857e9d05f3530bc544ca0cc102ce53
Bryan J Swift <bryan.j.swift@gmail.com>
2011-04-11 12:06:42 -0400

    Add hashCode, equals and toString to NoteList

--- src/main/java/com/bryanjswift/simplenote/model/NoteList.java
+++ src/main/java/com/bryanjswift/simplenote/model/NoteList.java
@@ -1,6 +1,7 @@
 package com.bryanjswift.simplenote.model;
 
 import com.bryanjswift.simplenote.Constants;
+import com.google.common.base.Objects;
 import com.google.common.collect.ImmutableList;
 
 import java.util.List;
@@ -22,5 +23,31 @@ public class NoteList {
         final ImmutableList.Builder<Note> notesBuilder = ImmutableList.builder();
         notesBuilder.addAll(notes);
         this.notes = notesBuilder.build();
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hashCode(this.mark, this.count, this.notes);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        final boolean result;
+        if (o instanceof NoteList) {
+            final NoteList list = (NoteList) o;
+            result = Objects.equal(this.mark, list.mark) && Objects.equal(this.count, list.count) && Objects.equal(this.notes, list.notes);
+        } else {
+            result = false;
+        }
+        return result;
+    }
+
+    @Override
+    public String toString() {
+        final Objects.ToStringHelper helper = Objects.toStringHelper(this);
+        helper.add("mark", this.mark);
+        helper.add("count", this.count);
+        helper.addValue(this.notes);
+        return helper.toString();
     }
 }
