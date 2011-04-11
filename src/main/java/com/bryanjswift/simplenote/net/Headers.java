504dcbd983cca7e96e49e5c84884c30a6c234c82
Bryan J Swift <bryan.j.swift@gmail.com>
2011-04-11 04:17:55 -0400

    Add javadocs for methods and constructors

--- src/main/java/com/bryanjswift/simplenote/net/Headers.java
+++ src/main/java/com/bryanjswift/simplenote/net/Headers.java
@@ -6,13 +6,28 @@ import org.apache.http.Header;
 import java.util.EmptyStackException;
 import java.util.List;
 
-/** @author bryanjswift */
+/**
+ * Meant to mimic the interface of getting Header data from a org.apache.http.HttpResponse
+ *
+ * @author bryanjswift
+ */
 public class Headers {
     public static final Headers EMPTY = new Headers(new Header[] {});
     private final Header[] headers;
+
+    /**
+     * Create an instance of Headers from an Array of HTTP Headers
+     * @param headers to be stored for later access
+     */
     public Headers(final Header[] headers) {
         this.headers = headers;
     }
+
+    /**
+     * Get the value of the first Header encountered with a given name
+     * @param name of Header to retrieve value for
+     * @return first Header value encountered for provided name
+     */
     public String get(final String name) {
         for (final Header h : headers) {
             if (h.getName().equals(name)) {
@@ -21,6 +36,12 @@ public class Headers {
         }
         return null;
     }
+
+    /**
+     * Retrieve a List of values for a given Header name
+     * @param name of Headers to look for
+     * @return List of String values for a given header name
+     */
     public List<String> getAll(final String name) {
         final ImmutableList.Builder<String> b = ImmutableList.builder();
         for (final Header h : headers) {
