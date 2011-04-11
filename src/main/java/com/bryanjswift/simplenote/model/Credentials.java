babb136c07f6c7562c84056c299a123c0588e4f7
Bryan J Swift <bryan.j.swift@gmail.com>
2011-04-11 14:20:16 -0400

    Javadoccing for Credentials

--- src/main/java/com/bryanjswift/simplenote/model/Credentials.java
+++ src/main/java/com/bryanjswift/simplenote/model/Credentials.java
@@ -5,11 +5,24 @@ public class Credentials {
     public final String auth;
     public final String email;
     public final String password;
+
+    /**
+     * Construct credentials from all three fields
+     * @param auth token for communicating securely with API
+     * @param email user identifier sent to server
+     * @param password sent during the login procedure
+     */
     public Credentials(final String auth, final String email, final String password) {
         this.auth = auth;
         this.email = email;
         this.password = password;
     }
+
+    /**
+     * Create simple auth, without a password
+     * @param auth token for communicating securely with API
+     * @param email user identifier sent to server
+     */
     public Credentials(final String auth, final String email) {
         this(auth, email, null);
     }
